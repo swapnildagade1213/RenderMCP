3a5112bf6601babab01bcf4b0316045442cf6537
swapnildagade1213 <111967597+swapnildagade1213@users.noreply.github.com>
2026-04-06 23:47:18 +0530

    Update executor.py

--- app/executor.py
+++ app/executor.py
@@ -1,10 +1,15 @@
+
 # app/executor.py
+
+from app.tools.functions import echo, add
 
 TOOL_HANDLERS = {
     "echo": echo,
     "add": add,
 }
 
-
 def execute(tool_name: str, args: dict):
+    if tool_name not in TOOL_HANDLERS:
+        raise ValueError(f"Unknown tool: {tool_name}")
+
     return TOOL_HANDLERS[tool_name](**args)
