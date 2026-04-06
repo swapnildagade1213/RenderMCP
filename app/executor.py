8ec9c7cba8f75fcbac5cc38db27b3996e732c7a0
swapnildagade1213 <111967597+swapnildagade1213@users.noreply.github.com>
2026-04-06 23:39:29 +0530

    Update executor.py

--- app/executor.py
+++ app/executor.py
@@ -1,21 +1,10 @@
 # app/executor.py
 
-class AsyncToolExecutor:
-    async def execute(self, tool_name, params):
-        handler = self.handlers.get(tool_name)
-        return await handler(**params)
-
-    def register(self, name, handler):
-        if not hasattr(self, "handlers"):
-            self.handlers = {}
-        self.handlers[name] = handler
+TOOL_HANDLERS = {
+    "echo": echo,
+    "add": add,
+}
 
 
-class ToolInfo:
-    def __init__(self, tool, category="general", tags=None, version="1.0", author="", visible=True):
-        self.tool = tool
-        self.category = category
-        self.tags = tags or []
-        self.version = version
-        self.author = author
-        self.visible = visible
+def execute(tool_name: str, args: dict):
+    return TOOL_HANDLERS[tool_name](**args)
